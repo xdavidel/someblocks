d8eeaf15b9abb9df9597c94c6fb41d4d2d950c21
Raphael Robatsch <raphael-git@tapesoftware.net>
2021-11-01 13:56:58 +0100

    add my personal config file

---
+++ blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+#define LAPTOP
+#ifdef LAPTOP
+    {"🔋 ", "cat /sys/class/power_supply/BAT0/capacity", 30, 0 },
+    {"🔊 ", "pulsemixer --get-mute --get-volume | awk 'NR==1 && /1/ { print \"x\"; exit } NR==2 { print $1 }'", 30, 1},
+#endif
+    {"", "date '+%a, %b %d %H:%M'",					                       5, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "    ";
+static unsigned int delimLen = 5;
